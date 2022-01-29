af6a8e48dac76092744a72b81e158781b8a437fd
corn1200 <corn120033@gmail.com>
2022-01-29 23:47:00 +0900

    티스토리 라이프 페이지의 게시물 정보를 불러옵니다.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-axios.get("http://example.com").then(response => {
-    const htmlString
-});
+axios
+  .get(
+    "https://www.tistory.com/category/getMoreCategoryPost.json?category=life&lastPublished=0&first=true"
+  )
+  .then((response) => {
+    const htmlString = response.data;
+    // 웹 사이트 로드
+    // const $ = cheerio.load(htmlString);
+    // const href = $('a').attr('href');
+    console.dir(htmlString.data.list);
+  });

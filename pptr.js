b65435be98144f3ea5a6fc819e200161198019eb
corn1200 <corn120033@gmail.com>
2022-01-30 02:04:15 +0900

    특정 게시물 목록의 text들을 가져옵니다

--- pptr.js
+++ pptr.js
@@ -1,16 +1,20 @@
 const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
   });
-
   const page = await browser.newPage();
   await page.setViewport({
     width: 1440,
     height: 1080,
   });
-  await page.goto("https://www.tistory.com/category/life");
-  //   await page.screenshot({ path: "example.png" });
-  //   await browser.close();
+  await page.goto("https://www.tistory.com/category/life", {
+    waitUntil: "networkidle2",
+  });
+  const html = await page.content();
+  const $ = cheerio.load(html);
+  const articles = $("ul.list_tistory > li > a").text();
+  console.log(articles);
 })();

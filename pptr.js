8c2b724f59264f3b9f34f7b06f8abc3bba2b415d
corn1200 <corn120033@gmail.com>
2022-01-30 02:24:54 +0900

    제목 값을 추가하여 오브젝트 형태로 배열에 삽입합니다

--- pptr.js
+++ pptr.js
@@ -16,12 +16,17 @@ const cheerio = require("cheerio");
     });
     const html = await page.content();
     const $ = cheerio.load(html);
-    // a 태그의 href 값들을 담을 배열
+    // a 태그의 href, 제목 값들을 담을 배열
     let hrefArray = [];
-    // 가져온 엘리먼트들을 순회하며 href 값을 배열에 저장합니다.
+    // 가져온 엘리먼트들을 순회하며 href, 제목 값을 배열에 저장합니다.
     const articles = $("ul.list_tistory > li > a").each((index, element) => {
         const href = $(element).attr("href");
-        hrefArray.push(href);
+        const title = $(element).find(".inner_desc_tit").text();
+        // 배열에 오브젝트 형태로 삽입합니다.
+        hrefArray.push({
+            href,
+            title,
+        });
     });
     console.log(hrefArray);
 })();

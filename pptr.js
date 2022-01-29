27cee30d3b9577c8f3bb089b6041c40c4b5cf231
corn1200 <corn120033@gmail.com>
2022-01-30 01:13:21 +0900

    퍼펫티어 런치 설정을 수정하여 브라우저 창이 뜨게 하고 브라우저 크기를 설정합니다

--- pptr.js
+++ pptr.js
@@ -1,9 +1,16 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+
   const page = await browser.newPage();
-  await page.goto("https://example.com");
-  await page.screenshot({ path: "example.png" });
-  await browser.close();
+  await page.setViewport({
+    width: 1440,
+    height: 1080,
+  });
+  await page.goto("https://www.tistory.com/category/life");
+  //   await page.screenshot({ path: "example.png" });
+  //   await browser.close();
 })();

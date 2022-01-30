55b434c981fbe070d19cd0312040decf1b43e53e
corn1200 <corn120033@gmail.com>
2022-01-31 01:27:13 +0900

    인라인으로 작성 된 API를 파라미터를 오브젝트로 넘겨주는 형태로 수정합니다

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const crawler = (pageNumber) => {
   return (
     axios
       // 브런치의 첫 페이지를 가져옵니다
-      .get(
-        `https://api.brunch.co.kr/v1/search/article?q=IT&page=${pageNumber}&pageSize=20&highlighter=y&escape=y&sortBy=accu`
-      )
+      .get("https://api.brunch.co.kr/v1/search/article", {
+        params: {
+          q: "IT",
+          page: pageNumber,
+          pageSize: 20,
+          highlighter: "y",
+          escape: "y",
+          sortBy: "accu",
+        },
+      })
       // 10 페이지가 될 때 까지 재귀반복하며 데이터를 오브젝트로 저장합니다
       .then((response) => {
         const data = response.data;

0d96273d1301c5630bed37537d3293d13d5cc36f
corn1200 <corn120033@gmail.com>
2022-01-30 22:24:51 +0900

    브런치 무한 스크롤 페이지의 api로 원하는 페이지만큼의 데이터를 불러옵니다

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-axios
-  .get(
-    "https://www.tistory.com/category/getMoreCategoryPost.json?category=life&lastPublished=0&first=true"
-  )
-  .then((response) => {
-    const htmlString = response.data;
-    // 웹 사이트 로드
-    // const $ = cheerio.load(htmlString);
-    // const href = $('a').attr('href');
-    console.dir(htmlString.data.list);
-  });
+let article = {};
+const crawler = (pageNumber) => {
+    return axios
+        // 브런치의 첫 페이지를 가져 옵니다
+        .get(
+            `https://api.brunch.co.kr/v1/search/article?q=IT&page=${pageNumber}&pageSize=20&highlighter=y&escape=y&sortBy=accu`
+        )
+        // 10 페이지가 될 때 까지 재귀반복하며 데이터를 오브젝트로 저장하고
+        // 오브젝트 구조를 출력합니다
+        .then((response) => {
+            const data = response.data;
+            article[pageNumber] = data.data.list;
+            console.log("current page number:", pageNumber);
+            const nextNumber = pageNumber + 1;
+            if (nextNumber < 10) {
+                crawler(nextNumber);
+                return;
+            }
+            console.log(article);
+        });
+};
+
+const data = crawler(1);

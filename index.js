f25a8d25469863586cc617a1e74e9d59695181ed
corn1200 <corn120033@gmail.com>
2022-01-31 00:47:09 +0900

    api를 사용하여 재귀적으로 다음 페이지의 정보를 불러옵니다

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 const axios = require("axios");
-const cheerio = require("cheerio");
+const fs = require("fs");
 
 let article = {};
 const crawler = (pageNumber) => {
-    return axios
-        // 브런치의 첫 페이지를 가져 옵니다
-        .get(
-            `https://api.brunch.co.kr/v1/search/article?q=IT&page=${pageNumber}&pageSize=20&highlighter=y&escape=y&sortBy=accu`
-        )
-        // 10 페이지가 될 때 까지 재귀반복하며 데이터를 오브젝트로 저장하고
-        // 오브젝트 구조를 출력합니다
-        .then((response) => {
-            const data = response.data;
-            article[pageNumber] = data.data.list;
-            console.log("current page number:", pageNumber);
-            const nextNumber = pageNumber + 1;
-            if (nextNumber < 10) {
-                crawler(nextNumber);
-                return;
-            }
-            console.log(article);
+  return (
+    axios
+      // 브런치의 첫 페이지를 가져옵니다
+      .get(
+        `https://api.brunch.co.kr/v1/search/article?q=IT&page=${pageNumber}&pageSize=20&highlighter=y&escape=y&sortBy=accu`
+      )
+      // 10 페이지가 될 때 까지 재귀반복하며 데이터를 오브젝트로 저장합니다
+      .then((response) => {
+        const data = response.data;
+        article[pageNumber] = data.data.list.map((item) => {
+          return {
+            title: item.title,
+            contentSummary: item.contentSummary,
+            contentId: item.contentId,
+          };
         });
+        console.log("current page number:", pageNumber);
+        const nextNumber = pageNumber + 1;
+        if (nextNumber < 10) {
+          crawler(nextNumber);
+          return;
+        }
+        // 오브젝트를 json 파일로 저장합니다
+        fs.writeFile(
+          "brunch_article.json",
+          JSON.stringify(article),
+          (err, data) => {
+            // 오류가 존재할 경우 오류 로그를 찍습니다.
+            if (err) {
+              console.error(err);
+              return;
+            }
+            console.log("success file write");
+          }
+        );
+      })
+  );
 };
 
 const data = crawler(1);
